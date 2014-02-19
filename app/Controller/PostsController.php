99b2e9b170a37d479c38c3bda7f7b75bbfb18483
Javier Liarte <jliarte@gmail.com>
2014-02-19 10:12:49 +0100

    Added view() method to Post controller to fetch just one post by id

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,4 +5,8 @@ class PostsController extends AppController {
 	public function index() {
 		$this->set('posts', $this->Post->getAllPosts());
 	}
+
+	public function view($id = null) {
+		$this->set('post', $this->Post->getSinglePost($id));
+	}
 }
--- app/Test/Case/Controller/PostControllerTest.php
+++ app/Test/Case/Controller/PostControllerTest.php
@@ -17,4 +17,22 @@ class PostControllerTest extends ControllerTestCase {
 		$result = Hash::extract($this->vars['posts'], '{n}.Post[id=2]');
 		$this->assertEquals ($expected, $result);
 	}
+
+	/**
+	 * @test
+	 */
+	public function viewActionSetsPostVarWithRequestedId() {
+		$this->testAction('/posts/view/3');
+		$this->assertInternalType('array', $this->vars['post']);
+		$expected = array (
+			'Post' => array (
+				'id' => '3',
+				'title' => 'Title strikes back',
+				'body' => 'This is really exciting! Not.',
+				'created' => '2012-07-04 10:43:23',
+				'updated' => '2012-07-04 10:45:31'
+			)
+		);
+		$this->assertEquals($expected, $this->vars['post']);
+	}
 }
